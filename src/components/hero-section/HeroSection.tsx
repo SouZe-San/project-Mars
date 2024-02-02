5629d62b16ea8df52affb8549912de7d8431d7f0
SouZe-San <2003soumyajit@gamil.com>
2024-02-02 23:24:18 +0530

    model-add-NOT-Satisfy

--- src/components/hero-section/HeroSection.tsx
+++ src/components/hero-section/HeroSection.tsx
@@ -1,16 +1,31 @@
 // import React from 'react'
+import { Suspense} from "react";
 import "./hero_style.scss";
+import { Environment,OrbitControls,  } from "@react-three/drei";
+import { Model } from "../mars_model/Model.tsx";
+
+import { Canvas} from '@react-three/fiber'
+
 const HeroSection = () => {
+
   return (
     <div id="heroSection">
       {/* Background Circles */}
       <div className="absolute left_blob blob"></div>
       <div className="absolute right_blob blob"></div>
+      {/* 3d Marse  */}
+      <Canvas camera={{ position: [0, 0, 3] }}  id="Canvas">
+          <Suspense fallback={null}>
+            {/* <ambientLight intensity={0.1} /> */}
+            <Model/>
+            {/* minPolarAngle={Math.PI / 2.5} maxPolarAngle={Math.PI / 2.55}  */}
+          <OrbitControls enablePan={false}/>
+             <Environment preset="sunset" background={false}  />
+          </Suspense>
+      </Canvas>
       {/* Head Line */}
       <h1 className="headTAG">MARS</h1>
 
-      {/* 3d Marse */}
-      <canvas id="canvas" className="absolute"></canvas>
 
       {/* Other decoration */}
 
