c558d813af3be6b0c53c7325ce76d150ad169dd5
SouZe-San <2003soumyajit@gamil.com>
2024-03-02 13:03:05 +0530

    slide-gallery Done

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./gallery_style.scss";
 // Images Importing
 import img1 from "../../assets/gallery_image/mars_1.jpeg";
@@ -13,27 +13,45 @@ import img4 from "../../assets/gallery_image/mars_4.webp";
 const Gallery = ({ whatBtnClicked }: { whatBtnClicked: btnProps }) => {
   const images = [img1, img2, img4, img3, img1, img2, img4];
 
-  const [hoveredIndex, setHoveredIndex] = useState<number>(1);
-  // const [currentHovered, setCurrentHovered] = useState<number>(1);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const [currentHovered, setCurrentHovered] = useState<number>(1);
+  const [hoveredIndex, setHoveredIndex] = useState<number>(currentHovered);
 
   const handleHover = (index: number) => {
     setHoveredIndex(index);
   };
 
   const handleLeave = () => {
-    setHoveredIndex(1);
-    // setHoveredIndex(currentHovered);
+    // setHoveredIndex(1);
+    setHoveredIndex(currentHovered);
   };
 
   useEffect(() => {
-    console.log(whatBtnClicked);
-  }, [whatBtnClicked]);
+    if (containerRef.current) {
+      if (whatBtnClicked.whatSideClicked === "rightBtn") {
+        containerRef.current.style.transform = `translateX(-${whatBtnClicked.posNumber * 15}%)`;
+        setCurrentHovered(hoveredIndex + 1);
+        setHoveredIndex(hoveredIndex + 1);
+      } else if (whatBtnClicked.whatSideClicked === "leftBtn") {
+        const currentTransform = containerRef.current.style.transform;
+
+        // Extract the translateX value from the current transform
+        const match = currentTransform.match(/translateX\(([-0-9]+)%\)/);
+        const currentTranslateX = match ? parseFloat(match[1]) : 0;
+
+        containerRef.current.style.transform = `translateX(${currentTranslateX + 15}%)`;
+        setCurrentHovered(hoveredIndex - 1);
+        setHoveredIndex(hoveredIndex - 1);
+      }
+    }
+  }, [hoveredIndex, whatBtnClicked]);
 
   return (
     <div className="h-full overflow-hidden relative">
       <h1 className="header_tag mb-12">Gallery or Marsian Life</h1>
 
-      <div className="card_container relative flex justify-center">
+      <div className="card_container relative flex justify-center" ref={containerRef}>
         {images.map((image, index) => {
           return (
             <div
@@ -41,7 +59,6 @@ const Gallery = ({ whatBtnClicked }: { whatBtnClicked: btnProps }) => {
               className={`card ${hoveredIndex === index ? "currentHovered" : ""}`}
               onMouseEnter={() => handleHover(index)}
               onMouseLeave={handleLeave}
-              // onChange={() => setHoveredIndex(currentHovered)}
             >
               <h3 className="card_no">00{index + 1}</h3>
               <img src={image} alt="mars" />
